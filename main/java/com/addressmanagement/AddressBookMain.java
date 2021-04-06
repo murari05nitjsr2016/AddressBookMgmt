c54897fc825d823f80d280db1d42ff0bb5d46564
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-06 22:04:55 +0530

    Welcome Program

---
+++ main/java/com/addressmanagement/AddressBookMain.java
@@ -0,0 +1,8 @@
+package com.addressmanagement;
+
+public class AddressBookMain {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Address Book Program");
+    }
+}
